bbc08c2563ba281e45a7d3b1e02903283d1b4074
guilhermeonrails <30351153+guilhermeonrails@users.noreply.github.com>
2024-08-30 15:33:15 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    // Inicializa uma string vazia para armazenar os resultados
+    let resultados = "";
+
+    // Itera sobre cada dado da lista de dados
+    for (let dado of dados) {
+        // Cria um novo elemento HTML para cada resultado
+        resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href=${dado.link} target="_blank">Mais informações</a>
+            </div>
+        `;
+    }
+
+    // Atribui os resultados gerados à seção HTML
+    section.innerHTML = resultados;
+}
